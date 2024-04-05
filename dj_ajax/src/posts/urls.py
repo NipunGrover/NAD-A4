66df31ccfce35191519aeb67caeffcf1f94fe28c
NipunGrover <ngrover5702@conestogac.on.ca>
2024-04-05 18:01:54 -0400

    made the root of the website to display main page where all posts and description are listed

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path 
 from .views import (
-    por_list_and_create,
+    post_list_and_create,
 )
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('', por_list_and_create, name='main-board'),
+    path('', post_list_and_create, name='main-board'),
 ]
