72ddb2e1b5b9f022f06296b9cca61a2447e6e3cc
NipunGrover <ngrover5702@conestogac.on.ca>
2024-04-05 19:10:22 -0400

    added console log with ajax call

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,4 +1,18 @@
 console.log("hello world")
 
 const hellowWorldBox = document.getElementById("hello-world")
-hellowWorldBox.textContent = "Hello World!"
+
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response)
+    {
+        console.log('success', response.text)
+    },
+    errror: function(error)
+    {
+        console.log('error', error)
+    }
+
+})
