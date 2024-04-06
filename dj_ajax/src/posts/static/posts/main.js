f85e8d54a34af55058c66098137727f996c10a6f
NipunGrover <ngrover5702@conestogac.on.ca>
2024-04-05 20:40:25 -0400

    console logging and displaying posts objects on the main page

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,7 +1,7 @@
 console.log("hello world")
 
 const hellowWorldBox = document.getElementById("hello-world")
-
+const postsBox = document.getElementById("posts-box")
 
 $.ajax({
     type: 'GET',
@@ -9,10 +9,30 @@ $.ajax({
     success: function(response)
     {
         console.log('success', response.text)
+        hellowWorldBox.textContent = response.text
     },
     errror: function(error)
     {
         console.log('error', error)
     }
 
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response)
+    {
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br> 
+            `   
+        });
+    },
+    error: function(error)
+    {
+        console.log(error)
+    }
 })
