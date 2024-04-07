67f2d0461f51fa90c596bc2df32f28d0ef6019c1
NipunGrover <ngrover5702@conestogac.on.ca>
2024-04-06 20:14:17 -0400

    showing update and delete button only for author

--- dj_ajax/src/posts/static/posts/detail.js
+++ dj_ajax/src/posts/static/posts/detail.js
@@ -1,5 +1,42 @@
 const backBtn = document.getElementById('back-btn');
+const updatBtn = document.getElementById('update-btn');
+const deleteBtn = document.getElementById('delete-btn');
+const url = window.location.href + "data/";
+const spinnerBox = document.getElementById('spinner-box')
 
 backBtn.addEventListener('click', () => {
     history.back();
 })
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response)
+    {
+        console.log(response)
+        spinnerBox.classList.add('not-visible')
+        const data = response.data
+
+        if (data.logged_in === data.author) {
+            updatBtn.classList.remove('not-visible')
+            deleteBtn.classList.remove('not-visible')
+        }
+        // response.forEach(el => {
+        //     document.getElementById('posts').innerHTML += `
+        //         <div class="card mt-3">
+        //             <div class="card-header">
+        //                 ${el.title}
+        //             </div>
+        //             <div class="card-body">
+        //                 <p class="card-text">${el.body}</p>
+        //             </div>
+        //         </div>
+        //     `
+        //})
+    },
+    error: function(error)
+    {
+        console.log(error)
+    }
+})
+
