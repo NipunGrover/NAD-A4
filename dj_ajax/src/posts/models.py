cab74c8f9724e3d1ba988a255f0dc28c98bf8645
NipunGrover <ngrover5702@conestogac.on.ca>
2024-04-06 22:13:45 -0400

    added funcitonality to display photos is carousel

--- dj_ajax/src/posts/models.py
+++ dj_ajax/src/posts/models.py
@@ -20,5 +20,18 @@ class Post(models.Model):
         return self.liked.all().count() 
     
 
+    def get_photos(self):
+        return self.photo_set.all()
+
+
     class Meta:
         ordering = ("-created",)
+
+
+class Photo(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.post.title}-{self.pk} "
